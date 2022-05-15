e44f5f6e778ba83af3e7a09c313e6400ff22d186
NaveenRavi <gamerkidnav@gmail.com>
2022-05-15 17:22:17 +0530

    Full err Going Back

--- dashboard/routes/admin.js
+++ dashboard/routes/admin.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('admin/home')
 });
-
+router.get('/status',async(req,res)=>{
+  res.render('admin/status')
+})
 module.exports = router;
